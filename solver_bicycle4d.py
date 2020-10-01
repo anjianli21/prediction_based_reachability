fix bicycle 4d brs computation

solver_bicycle4d.py
@@ -112,8 +112,14 @@ def main():
              print("Computational time to integrate (s): {:.5f}".format(time.time() - start))
 
              print("tNow is ", tNow)
+
              # Saving reachable set and control data into disk
-             if tNow == 0.00516090914607048 or tNow == 1 or tNow == 2 or tNow == 3:
+             if tNow == 0.00516090914607048 or tNow == 2 or tNow == 3 or tNow == 4:
+                print("Max diff. from previous time step: {}".format(np.max(np.abs(V_1.asnumpy() - V1_old))))
+                print("Avg diff. from previous time step: {}".format(np.mean(np.abs(V_1.asnumpy() - V1_old))))
+
+                # TODO: change scenario here
+                # Intersection ###############################################################################
                 file_dir = '/home/anjianl/Desktop/project/optimized_dp/data/brs/0929/obstacle/intersection'
                 if not os.path.exists(file_dir):
                     os.mkdir(file_dir)
@@ -129,8 +135,25 @@ def main():
                 np.save(file_ctrl_acc_path % tNow, uOpt_2.asnumpy())
                 print("intersection control acc is saved!")
 
+                # # Roundabout ##################################################################################
+                # file_dir = '/home/anjianl/Desktop/project/optimized_dp/data/brs/0929/obstacle/roundabout'
+                # if not os.path.exists(file_dir):
+                #     os.mkdir(file_dir)
+                # file_brs_path = file_dir + '/bicycle4d_brs_roundabout' + '_t_%.2f.npy'
+                # np.save(file_brs_path % tNow, V_1.asnumpy())
+                # print("roundabout brs is saved!")
+                #
+                # # Save control data
+                # file_ctrl_beta_path = file_dir + '/bicycle4d_roundabout_ctrl_beta' + '_t_%.2f.npy'
+                # file_ctrl_acc_path = file_dir + '/bicycle4d_roundabout_ctrl_acc' + '_t_%.2f.npy'
+                # np.save(file_ctrl_beta_path % tNow, uOpt_1.asnumpy())
+                # print("roundabout control beta is saved!")
+                # np.save(file_ctrl_acc_path % tNow, uOpt_2.asnumpy())
+                # print("roundabout control acc is saved!")
+
         print("Max diff. from previous time step: {}".format(np.max(np.abs(V_1.asnumpy() - V1_old))))
         print("Avg diff. from previous time step: {}".format(np.mean(np.abs(V_1.asnumpy() - V1_old))))
+
     # Time info printing
     print("Total kernel time (s): {:.5f}".format(execution_time))
     print("Finished solving\n")

user_definer.py
@@ -78,9 +78,12 @@ print("Computing bicycle dynamics 4D intersection")
 
 # Use the grid to initialize initial value function
 Initial_value_f = np.load("/home/anjianl/Desktop/project/optimized_dp/data/map/value_function/intersection_valfunc_correct.npy") # Load the fmm map-based value function of the obstacle
+# Add speed target
+Initial_value_f = np.minimum(Initial_value_f, HalfPlane(g, 0, 3))
+Initial_value_f = np.minimum(Initial_value_f, - HalfPlane(g, 17, 3))
 
 # Look-back length and time step
-lookback_length = 3.1
+lookback_length = 4.1
 t_step = 0.05
 
 small_number = 1e-5
@@ -89,7 +92,7 @@ print("Welcome to optimized_dp \n")
 
 # Use the following variable to specify the characteristics of computation
 # compMethod = "none" # Reachable set
-compMethod = ["minVWithV0", "maxVWithCStraint"] # Reachable tube with obstacles
+compMethod = "minVWithV0" # Reachable tube
 
 my_object  = my_car
 my_shape = Initial_value_f
@@ -110,11 +113,12 @@ my_shape = Initial_value_f
 #
 # # Use the grid to initialize initial value function
 # Initial_value_f = np.load("/home/anjianl/Desktop/project/optimized_dp/data/map/value_function/roundabout_valfunc_correct.npy") # Load the fmm map-based value function of the obstacle
+# # Add speed target
 # Initial_value_f = np.minimum(Initial_value_f, HalfPlane(g, 0, 3))
 # Initial_value_f = np.minimum(Initial_value_f, - HalfPlane(g, 17, 3))
 #
 # # Look-back length and time step
-# lookback_length = 3.1
+# lookback_length = 4.1
 # t_step = 0.05
 #
 # small_number = 1e-5
@@ -123,7 +127,7 @@ my_shape = Initial_value_f
 #
 # # Use the following variable to specify the characteristics of computation
 # # compMethod = "none" # Reachable set
-# compMethod = ["minVWithV0", "maxVWithCStraint"] # Reachable tube with obstacles
+# compMethod = "minVWithV0" # Reachable tube
 #
 # my_object  = my_car
 # my_shape = Initial_value_f